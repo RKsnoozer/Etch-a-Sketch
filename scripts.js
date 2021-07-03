7058111694708c70109a207561cd093202c8de54
RKsnoozer <RKsnoozer@Gmail.com>
2021-07-03 19:37:09 +0100

    Copying 1.1

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
-const matrix = document.getElementById("container")
+const container = document.getElementById("container");
+const title = document.getElementById("Title");
+title.style.color = "red";
+
+function setDefaultGrid() {
+    setGridSize(16);
+    createGrid(16);
+}
+
+function setGridSize() {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`; 
+}
 
 function createGrid (gridSize) {
     let gridArea = gridSize * gridSize;
     for (let i = 1; i < gridArea; i++) {
-        let matrix = document.createElement("div");
+        const matrix = document.createElement("div");
+        matrix.classList = "grid-element";
+        matrix.addEventListener("mouseover");
+        container.appendChild(matrix);
     }
 }
+
